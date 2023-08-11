2b0a0eef315bc17cde89d3d63f39c7c8324a1b28
devansh.jain <devansh.jain@G1-BWXRM13-L>
2023-08-11 07:45:08 +0530

    Added  Math Class

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,19 @@
-﻿using FirstConsole;
+﻿using ConsoleApp1;
+using FirstConsole;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+        /*
         Class1 c = new Class1();
         c.myLocalVar = "New Var";
         c.sharpen();
         Console.WriteLine(c.myLocalVar);
         Console.WriteLine(c.MakePencil(c.myLocalVar.Length));
+        */
+
+        hwDay1 h1 = new hwDay1();
+        h1.mathClass(4,3);
     }
 }
---
+++ ConsoleApp1/hwDay1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class hwDay1
+    {
+        // Math Class
+        public void mathClass(int x,int y)
+        {
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("y = " + y);
+            Console.WriteLine("Math.E = " + Math.E);
+            Console.WriteLine("Math.PI = " + Math.PI);
+            Console.WriteLine("Math.Pow = " + Math.Pow(x,3));
+            Console.WriteLine("Math.Sqrt = " + Math.Sqrt(x));
+            Console.WriteLine("Math.Sin = " + Math.Sin(x));
+            Console.WriteLine("Math.Equals = " + Math.Equals(x,3));
+            Console.WriteLine("Math.Max = " + Math.Max(x, y));
+        }
+
+    }
+}
